31af5709ccb05bded982eda2f852c2ab674f976e
Ben Godden <linguaenthusiast@gmail.com>
2023-02-27 21:18:57 -0700

    disabled buttons

--- ankiGrab.js
+++ ankiGrab.js
@@ -3,6 +3,7 @@ let english = "";
 let exportText = "";
 let url = window.location.href;
 let buttonDefault = "padding: 10px 10px;";
+let exportable = false;
 
 const container = document.createElement("div");
 container.setAttribute("id", "div-container");
@@ -32,7 +33,7 @@ container.appendChild(buttonEnglish);
 const buttonAdd = document.createElement("Button");
 buttonAdd.setAttribute("id", "log-text");
 buttonAdd.innerHTML = "Log";
-buttonAdd.style = "";
+// buttonAdd.style = "background-color: #5e5a5a;";
 buttonAdd.onclick = function () {
   addToList();
 };
@@ -41,7 +42,9 @@ container.appendChild(buttonAdd);
 const buttonExport = document.createElement("Button");
 buttonExport.setAttribute("id", "export-text");
 buttonExport.innerHTML = "Export";
-buttonExport.style = "";
+// buttonExport.style = "background-color: #5e5a5a;";
+// Need to add a disabled class on button Export that disables it until it is enabled with a call from buttonAdd (#log-text)
+// document.getElementById("Button").disabled = true;
 buttonExport.onclick = function () {
   export2Anki();
 };
@@ -64,8 +67,14 @@ btnBaseCss.innerHTML =
 document.getElementsByTagName("head")[0].appendChild(btnBaseCss);
 document.getElementById("get-cree").className = "cssClass";
 document.getElementById("get-english").className = "cssClass";
-document.getElementById("log-text").className = "cssClass";
-document.getElementById("export-text").className = "cssClass";
+
+let btnDisabled = document.createElement("style");
+btnDisabled.type = "text/css";
+btnDisabled.innerHTML =
+  ".cssClassDisabled { margin-bottom: 0;font-size: 14px;font-weight: 400;line-height: 1.42857143;text-align: center;white-space: nowrap;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;padding: 10px 15px;font-size: 14px;text-align: center;outline: none;color: #fff;background-color: #5e5a5a;border: none;border-radius: 15px;box-shadow: 0 4px #999; }";
+document.getElementsByTagName("head")[0].appendChild(btnDisabled);
+document.getElementById("export-text").className = "cssClassDisabled";
+document.getElementById("log-text").className = "cssClassDisabled";
 
 function getSelectionText() {
   let text = "";
@@ -83,7 +92,8 @@ function getCree() {
     buttonCree.style = "background-color:#4CAF50;";
     console.log("Current Cree Queue: ", cree);
     if (english != "") {
-      buttonAdd.style = "background-color:#4CAF50;";
+      document.getElementById("log-text").className = "cssClass";
+      document.getElementById("log-text").classList.remove("cssClassDisabled");
     }
   }
 }
@@ -94,7 +104,8 @@ function getEnglish() {
     buttonEnglish.style = "background-color:#4CAF50;";
     console.log("Current English Queue: ", english);
     if (cree != "") {
-      buttonAdd.style = "background-color:#4CAF50;";
+      document.getElementById("log-text").className = "cssClass";
+      document.getElementById("log-text").classList.remove("cssClassDisabled");
     }
   }
 }
@@ -104,11 +115,14 @@ function addToList() {
     exportText += `${cree}\|${english}\|${url}\n`;
     buttonCree.style = buttonDefault;
     buttonEnglish.style = buttonDefault;
-    buttonAdd.style = "";
-    buttonExport.style = "background-color:#8022b3;";
+    buttonAdd.style = "background-color: #5e5a5a;";
+    document.getElementById("export-text").classList.remove("cssClassDisabled");
+    document.getElementById("export-text").className = "cssClass";
+    // document.getElementById("export-text").disabled = false;
     console.log(`Added to Log: \n ${cree}\|${english}\|${url}\n`);
     cree = "";
     english = "";
+    exportable = true;
   } else {
     alert(
       "You must save both Cree Text and English Text in order to be able to log the data."
@@ -117,17 +131,21 @@ function addToList() {
 }
 
 function export2Anki() {
-  let element = document.createElement("a");
-  element.setAttribute(
-    "href",
-    "data:text/plain;charset=utf-8," + encodeURIComponent(exportText)
-  );
-  element.setAttribute("download", "cardsForAnki.txt");
-  element.style.display = "none";
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
-  buttonExport.style = "";
+  if (exportable == true) {
+    let element = document.createElement("a");
+    element.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-8," + encodeURIComponent(exportText)
+    );
+    element.setAttribute("download", "cardsForAnki.txt");
+    element.style.display = "none";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+    exportable = false;
+    document.getElementById("export-text").className = "cssClass";
+    document.getElementById("export-text").classList.remove("cssClassDisabled");
+  }
 }
 
 function quitting() {
--- ankiGrab_bookmarklet.js
+++ ankiGrab_bookmarklet.js
@@ -4,6 +4,7 @@ javascript: (() => {
   let exportText = "";
   let url = window.location.href;
   let buttonDefault = "padding: 10px 10px;";
+  let exportable = false;
 
   const container = document.createElement("div");
   container.setAttribute("id", "div-container");
@@ -33,7 +34,6 @@ javascript: (() => {
   const buttonAdd = document.createElement("Button");
   buttonAdd.setAttribute("id", "log-text");
   buttonAdd.innerHTML = "Log";
-  buttonAdd.style = "";
   buttonAdd.onclick = function () {
     addToList();
   };
@@ -42,7 +42,6 @@ javascript: (() => {
   const buttonExport = document.createElement("Button");
   buttonExport.setAttribute("id", "export-text");
   buttonExport.innerHTML = "Export";
-  buttonExport.style = "";
   buttonExport.onclick = function () {
     export2Anki();
   };
@@ -65,8 +64,14 @@ javascript: (() => {
   document.getElementsByTagName("head")[0].appendChild(btnBaseCss);
   document.getElementById("get-cree").className = "cssClass";
   document.getElementById("get-english").className = "cssClass";
-  document.getElementById("log-text").className = "cssClass";
-  document.getElementById("export-text").className = "cssClass";
+
+  let btnDisabled = document.createElement("style");
+  btnDisabled.type = "text/css";
+  btnDisabled.innerHTML =
+    ".cssClassDisabled { margin-bottom: 0;font-size: 14px;font-weight: 400;line-height: 1.42857143;text-align: center;white-space: nowrap;vertical-align: middle;-webkit-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;padding: 10px 15px;font-size: 14px;text-align: center;outline: none;color: #fff;background-color: #5e5a5a;border: none;border-radius: 15px;box-shadow: 0 4px #999; }";
+  document.getElementsByTagName("head")[0].appendChild(btnDisabled);
+  document.getElementById("export-text").className = "cssClassDisabled";
+  document.getElementById("log-text").className = "cssClassDisabled";
 
   function getSelectionText() {
     let text = "";
@@ -84,7 +89,10 @@ javascript: (() => {
       buttonCree.style = "background-color:#4CAF50;";
       console.log("Current Cree Queue: ", cree);
       if (english != "") {
-        buttonAdd.style = "background-color:#4CAF50;";
+        document.getElementById("log-text").className = "cssClass";
+        document
+          .getElementById("log-text")
+          .classList.remove("cssClassDisabled");
       }
     }
   }
@@ -95,7 +103,10 @@ javascript: (() => {
       buttonEnglish.style = "background-color:#4CAF50;";
       console.log("Current English Queue: ", english);
       if (cree != "") {
-        buttonAdd.style = "background-color:#4CAF50;";
+        document.getElementById("log-text").className = "cssClass";
+        document
+          .getElementById("log-text")
+          .classList.remove("cssClassDisabled");
       }
     }
   }
@@ -105,11 +116,16 @@ javascript: (() => {
       exportText += `${cree}\|${english}\|${url}\n`;
       buttonCree.style = buttonDefault;
       buttonEnglish.style = buttonDefault;
-      buttonAdd.style = "";
-      buttonExport.style = "background-color:#8022b3;";
+      document
+        .getElementById("export-text")
+        .classList.remove("cssClassDisabled");
+      document.getElementById("export-text").className = "cssClass";
+      document.getElementById("log-text").classList.remove("cssClass");
+      document.getElementById("log-text").className = "cssClassDisabled";
       console.log(`Added to Log: \n ${cree}\|${english}\|${url}\n`);
       cree = "";
       english = "";
+      exportable = true;
     } else {
       alert(
         "You must save both Cree Text and English Text in order to be able to log the data."
@@ -118,31 +134,43 @@ javascript: (() => {
   }
 
   function export2Anki() {
-    let element = document.createElement("a");
-    element.setAttribute(
-      "href",
-      "data:text/plain;charset=utf-8," + encodeURIComponent(exportText)
-    );
-    element.setAttribute("download", "cardsForAnki.txt");
-    element.style.display = "none";
-    document.body.appendChild(element);
-    element.click();
-    document.body.removeChild(element);
-    buttonExport.style = "";
+    if (exportable == true) {
+      let element = document.createElement("a");
+      element.setAttribute(
+        "href",
+        "data:text/plain;charset=utf-8," + encodeURIComponent(exportText)
+      );
+      element.setAttribute("download", "cardsForAnki.txt");
+      element.style.display = "none";
+      document.body.appendChild(element);
+      element.click();
+      document.body.removeChild(element);
+      exportable = false;
+      document.getElementById("export-text").className = "cssClassDisabled";
+      document.getElementById("export-text").classList.remove("cssClass");
+    }
   }
 
   function quitting() {
-    document.getElementById("get-cree").remove();
-    document.getElementById("get-english").remove();
-    document.getElementById("log-text").remove();
-    document.getElementById("export-text").remove();
-    document.getElementById("button-quit").remove();
-    document.getElementById("div-container").remove();
-    delete window.cree;
-    delete window.english;
-    delete window.exportText;
-    delete window.url;
-    delete window.buttonDefault;
-    delete window.btnBaseCss;
+    if (exportable == true) {
+      let prompt = confirm(
+        "You currently have cards logged but not exported. Quit anyway?"
+      );
+
+      if (prompt != false) {
+        document.getElementById("get-cree").remove();
+        document.getElementById("get-english").remove();
+        document.getElementById("log-text").remove();
+        document.getElementById("export-text").remove();
+        document.getElementById("button-quit").remove();
+        document.getElementById("div-container").remove();
+        delete window.cree;
+        delete window.english;
+        delete window.exportText;
+        delete window.url;
+        delete window.buttonDefault;
+        delete window.btnBaseCss;
+      }
+    }
   }
 })();
